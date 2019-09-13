75b66a2826c5833db8d773d3cbf2f18de96612e6
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2019-09-13 12:37:00 -0400

    Add files via upload
    Added comments to code

--- Video Speedup.py
+++ Video Speedup.py
@@ -1,18 +1,19 @@
+# Get input for given variables
 n, p, k = map(int, input().split())
 t = list(map(int, input().split()))
 
-p /= 100
-i = 0
-T = t[i]
-j = i + 1
+p /= 100  # Convert percentage to decimal
+i = 0     # Index counter
+T = t[i]  # Add first segment to running total
+j = i + 1 # Shorthand since i+1 is used so much
 
-if t[i] == k or p == 0 or p == 100:
-    pass
+if t[i] == k or p == 0 or p == 100: 
+    pass  # If the video doesn't speed up, skip the else statement
 else:
-    while j < n:
+    while j < n: # Adding the segments to the running total
         T = T + (1 + p*j) * (t[j] - t[i])
         i+=1
         j+=1
 
-T = T + (1 + p*j) * (k - t[i])
+T = T + (1 + p*j) * (k - t[i]) # Needed to add the last segment to the running total
 print(T)
