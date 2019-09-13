fc28554f51b93467b640e33558c155f6150f6234
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2019-09-13 12:41:53 -0400

    Add files via upload
    Added comments

--- Cold-puter Science.py
+++ Cold-puter Science.py
@@ -1,4 +1,4 @@
-n = (int, input().split())
-tempList = list(map(int, input().split()))
-t = sum(1 for number in tempList if number < 0)
-print(t)
+n = (int, input().split()) # Input number of temperatures
+tempList = list(map(int, input().split())) # Input list of temperatures
+t = sum(1 for number in tempList if number < 0) # Add the temperatures below zero to the running total
+print(t) # Print number of subzero temperatures
