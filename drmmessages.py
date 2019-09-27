4f87bd1fb90f35e1783fd54a04b33f0f23d6007b
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2019-09-27 00:47:09 -0400

    Create drmmessages.py
    First go-through with the code

---
+++ drmmessages.py
@@ -0,0 +1,24 @@
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 
+           'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+           'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+           'V', 'W', 'X', 'Y', 'Z']
+
+input_text = input()
+
+last_half = input_text[(int(len(input_text)/2)):]
+first_half = input_text[:-(int(len(input_text)/2))]
+
+rotate_1 = 0
+rotate_2 = 0
+
+for i in range(len(first_half)):
+    rotate_1+=letters.index(first_half[i])
+    rotate_2+=letters.index(last_half[i])
+    
+final = ''
+
+for j in range(len(first_half)):
+    final+= letters[(((letters.index(first_half[j]) + rotate_1) % 26 + 
+                      (letters.index(last_half[j]   + rotate_2) % 26) % 26)]
+                     
+print(final)
