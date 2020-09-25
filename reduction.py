0594b7a01788daf347259996815b8fc9474eadfd
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-09-25 13:56:39 -0400

    Create reduction.py
    open.kattis.com/problems/reduction
    Simplified by Tatyanna Villegas

---
+++ reduction.py
@@ -0,0 +1,40 @@
+num_cases = int(input())
+
+for i in range(num_cases):
+
+    N, M, L = map(int, input().split())
+    agency_list = list() # Need to keep track of each agency
+
+    for j in range(L):
+        agencyname, cash = input().split(':') # Seperate agency's name from its prices
+        A, B = map(int, cash.split(',')) # Seperate the cash string into A and B
+        min_price = 0 # Initialize min_price
+        n = N # Need a temporary N (n) so we retain the intial value of N
+        
+        while n != M:
+            # What these next lines do is compare the price ratio of A and B, 
+            # and figure out what N would be if either halved or if reduced by 1
+            halved = n//2
+
+            if halved < M:
+                min_price += (n - M)*A
+                n = M
+            else:
+                price_B = B
+                price_A = (n-halved)*A
+                if price_B < price_A:
+                    min_price += price_B
+                    n = halved
+                else:
+                    min_price += price_A
+                    n = halved
+            
+        else: # This runs after the while loop ends, and appends output to list
+            agency_list.append(agencyname + " " + str(min_price))
+    # This sorts the list by numbers first then by the agency name
+    agency_list.sort(key = lambda ele: (int(ele.split()[1]), str.lower(ele)))
+    
+    print("Case " + str(i+1))
+
+    for element in agency_list:
+        print(element)
