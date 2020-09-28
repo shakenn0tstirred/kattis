fafa71336963111cfab864da5da7faaf05ab6788
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-09-28 02:46:32 -0400

    Create carrots.py
    open.kattis.com/problems/carrots

---
+++ carrots.py
@@ -0,0 +1,5 @@
+# Since I am literally outputting the second character from the
+# first line of input, this will be a short program. It also ignores their descriptions.
+
+N, P = map(int, input().split())
+print(P)
