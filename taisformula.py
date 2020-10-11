db85dbc558e8474412ef3d69e0e44332b1c69fce
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-10-10 22:39:36 -0400

    Create taisformula.py
    open.kattis.com/problems/taisformula

---
+++ taisformula.py
@@ -0,0 +1,23 @@
+trapezoid = 0.0 #Initialize running total
+
+for i in range(int(input())):
+    if i == 0: # Initialize starting value
+        initial = input().split()
+        milliseconds_initial = int(initial[0])
+        glucose_initial = float(initial[1])
+    else:
+        current = input().split()
+        milliseconds_current = int(current[0])
+        glucose_current = float(current[1])
+        
+        # Use given formula to calculate area of the trapezoid
+        trapezoid_current = glucose_current + glucose_initial
+        trapezoid_current /= 2
+        trapezoid_current *= milliseconds_current - milliseconds_initial
+        trapezoid += trapezoid_current # Add derived area to running total
+        
+        # Set initial value to current total
+        milliseconds_initial = milliseconds_current
+        glucose_initial = glucose_current
+else:
+    print(trapezoid/1000) # Convert milliseconds to seconds upon printing running total
