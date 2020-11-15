b03956631421112675fd7e635965ee1688516fd0
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:45:08 -0500

    Update simonsays.py
    Added a simplified version utilizing the walrus operator introduced in Python 3.8

--- simonsays.py
+++ simonsays.py
@@ -1,4 +1,10 @@
+# Kattis-accepted version
 for i in range(int(input())):
     command = input()
     if "Simon says" in command:
+        print(command[11:])
+
+# Simplified version introduced in Python 3.8
+for i in range(int(input())):
+    if "Simon says" in (command:=input()):
         print(command[11:])
