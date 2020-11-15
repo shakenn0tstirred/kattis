326f895805fe31813cddda728322f38b5506635f
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:34:04 -0500

    Update ladder.py
    Simplified code by importing specific functions from the math library

--- ladder.py
+++ ladder.py
@@ -1,6 +1,6 @@
-import math
+from math import sin, radians, ceil
 answer = 0
 h, v = map(int, input().split())
 
-answer = h / math.sin(math.radians(v)) # height / sin(v) = ladder length
-print(math.ceil(answer)) # Round the answer up to the nearest whole number
+answer = h / sin(radians(v)) # height / sin(v) = ladder length
+print(ceil(answer)) # Round the answer up to the nearest whole number
