25cb1431415a932ae2a98e0e586172df8a8ffa58
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:35:26 -0500

    Update isithalloween.py
    simplified code by combining if and elif statements

--- isithalloween.py
+++ isithalloween.py
@@ -1,8 +1,6 @@
 date = input().split()
 
-if date[0] == "OCT" and date[1] == "31":
-    print("yup")
-elif date[0] == "DEC" and date[1] == "25":
+if date[0] == "OCT" and date[1] == "31" or date[0] == "DEC" and date[1] == "25":
     print("yup")
 else:
     print("nope")
