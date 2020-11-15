bf0eff953a39c0e91d0f0bf7980b498054169744
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:27:54 -0500

    Update oddities.py
    Simplified code

--- oddities.py
+++ oddities.py
@@ -1,7 +1,5 @@
-n = int(input())
-
 # Determines whether the input is even or odd
-for i in range(n):
+for i in range(int(input())):
     x = int(input())
     if x % 2 == 0:
         print(x, "is even")
