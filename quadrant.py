f8ac0464d9259bdc95e871fe87989116d30e2540
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:49:01 -0500

    Update quadrant.py
    Added a simplified version utilizing walrus operator

--- quadrant.py
+++ quadrant.py
@@ -1,7 +1,20 @@
+# Kattis-accepted code
 x = int(input())
 y = int(input())
 
 if x > 0 and y > 0:
+    print(1)
+elif x > 0:
+    print(4)
+elif y > 0:
+    print(2)
+else:
+    print(3)
+    
+    
+# Simplified version utilizing the walrus operator introduced in Python 3.8
+
+if (x:=int(input())) > 0 and (y:=int(input())) > 0:
     print(1)
 elif x > 0:
     print(4)
