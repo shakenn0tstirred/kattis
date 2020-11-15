61cec3b28fcd6a0a27edb23ceaa5eb50267be3ab
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:18:52 -0500

    Update aboveaverage.py
    Modified while loop to for loop to simplify the code

--- aboveaverage.py
+++ aboveaverage.py
@@ -1,6 +1,4 @@
-C = int(input())              # C is the number of test cases/data set inputs to follow
-i = 0                         # set index to zero
-while i < C:                  # Since C starts at 1 and i starts at 0, this will amount in C input cases
+for i in range(int(input())): # Iterate through given number of students
     grades = list(map(int, input().split()))
     N = grades[0]             # Keep the number of students
     del grades[0]             # Delete number of students from list of final grades
@@ -11,4 +9,3 @@ while i < C:                  # Since C starts at 1 and i starts at 0, this will
             del grades[grades.index(x)]
     aboveAverage = len(grades) / N # Compute percentage of students that scored above average
     print("{:.3%}".format(aboveAverage))
-    i += 1
