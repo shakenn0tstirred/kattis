7c098031ec237d2d4e6ce179caf136825788bc8b
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:51:19 -0500

    Update planina.py
    Simplified code

--- planina.py
+++ planina.py
@@ -1,10 +1,8 @@
 ##f(0) = 2
 ##f(x) = 2 * f(x-1) - 1
 
-iterations = int(input())
 answer = 2
-
-for i in range(iterations):
+for i in range(int(input())):
     answer = 2 * answer - 1
 
 print(answer**2)
