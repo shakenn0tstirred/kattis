ac8d1f34110008613413f4dc8eee055d9e75b58d
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:39:30 -0500

    Update speedlimit.py
    Simplified code by implementing a version utilizing the walrus operator introduced in Python 3.8

--- speedlimit.py
+++ speedlimit.py
@@ -1,3 +1,5 @@
+# Kattis-accepted implementation
+
 n = int(input())
 
 while n != -1:
@@ -10,3 +12,16 @@ while n != -1:
         time = h
     print(distance, "miles")
     n = int(input())
+
+    
+# Simplify code by implementing the walrus operator in Python 3.8+
+
+while (n:=int(input())) != -1:
+    time = 0
+    distance = 0
+
+    for i in range(n):
+        mph,h = map(int, input().split())
+        distance += (h - time) * mph
+        time = h
+    print(distance, "miles")
