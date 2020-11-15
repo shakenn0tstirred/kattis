5b4866fa6de3bbd1b3926b237f62d525d639d919
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:49:35 -0500

    Update qaly.py
    Simplified code

--- qaly.py
+++ qaly.py
@@ -1,6 +1,5 @@
-num_life_periods = int(input()) # Input number of life periods
 answer = 0 # Initialize answer variable
-for i in range(num_life_periods):
+for i in range(int(input())): # Loop through number of life periods
     q, y = map(float, input().split())
     answer += q * y # Keep a running total of the quantity of life periods
 print(answer)
