d943bcf3b56b5ccb0562615a6d7d1cb1b9804897
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-11-14 22:32:03 -0500

    Update lastfactorialdigit.py
    Simplified code; imported factorial instead of whole math library

--- lastfactorialdigit.py
+++ lastfactorialdigit.py
@@ -1,5 +1,3 @@
-import math # Need math library for factorial operation
-test_cases = int(input()) # Input number of test cases
-for i in range(test_cases):
-    num = math.factorial(int(input())) % 10 # takes the last digit of an integer
-    print(num)
+from math import factorial
+for i in range(int(input())):
+    print(factorial(int(input())) % 10) # takes the last digit of an integer
