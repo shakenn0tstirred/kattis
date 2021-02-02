0aaa7f8587ad2e2ebea3320ef7a4d75be65556fe
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:38:56 -0500

    Update drmmessages.py

--- drmmessages.py
+++ drmmessages.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 
            'H', 'I', 'J', 'K', 'L', 'M', 'N', 
            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
