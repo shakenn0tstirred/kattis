df3150ec434e06dfb7b37a8191cec1037ddc64c7
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 17:01:09 -0500

    Update r2.py

--- r2.py
+++ r2.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 r1, answer = map(int, input().split())
 # if R2/2 + R1/2 = answer, R2+R1 = 2*answer, and R2 = 2*answer - R1
 r2 = (2*answer) - r1
