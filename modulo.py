ef309e6f1dc6f6ba7bdc5ced3a660a2d140b4af6
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:58:00 -0500

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 numbers = [] # Initialize the input list
 
 # Use list comprehension to sort through the input
