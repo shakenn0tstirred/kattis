d9aa919375d4e56185ab33e1c56ee1bd8d207c06
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:41:24 -0500

    Update fiftyshades.py

--- fiftyshades.py
+++ fiftyshades.py
@@ -1,10 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 answer = 0
 for _ in range(int(input())):
     color = input()
 
     if "rose" in color.lower() or "pink" in color.lower():
         answer+=1
-
 
 if answer == 0:
     print("I must watch Star Wars with my daughter")
