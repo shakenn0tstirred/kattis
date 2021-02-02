b774fe3c385b0fac89e2d4aa2380b49742134ad6
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:38:30 -0500

    Update deathtaxes.py

--- deathtaxes.py
+++ deathtaxes.py
@@ -1,4 +1,6 @@
-# Accepted by Kattis
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 event = input().split() # Seperate input into lists
 
