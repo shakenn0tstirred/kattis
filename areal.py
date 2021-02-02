0d33a52a7c45c2cfe79e578ef834783c3a1e262a
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:13:21 -0500

    Update areal.py
    add license

--- areal.py
+++ areal.py
@@ -1,2 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 print(4*int(input())**0.5)
 # Input is area, length of a side is the square root of area, and perimeter is 4*length
