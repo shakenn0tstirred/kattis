d0546c0c25dd066566f833f6e001462ba865138b
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:40:56 -0500

    Update estimatingtheareaofacircle.py

--- estimatingtheareaofacircle.py
+++ estimatingtheareaofacircle.py
@@ -1,6 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from math import pi
 line = input().split()
-
 
 while line != ['0', '0', '0']:
     r = float(line[0])
