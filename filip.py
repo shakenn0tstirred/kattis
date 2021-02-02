8e0abf038a22c84c55a86e660206dee6275e91ca
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:42:25 -0500

    Update filip.py

--- filip.py
+++ filip.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 A, B = input().split() # Input two numbers as strings
 
 # Reverse both strings, convert into integer
