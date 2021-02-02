6dd2e25cea13da94808a00af5faf092fa685f1c4
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:37:27 -0500

    Update cold.py

--- cold.py
+++ cold.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 n = (int, input().split()) # Input number of temperatures
 tempList = list(map(int, input().split())) # Input list of temperatures
 t = sum(1 for number in tempList if number < 0) # Add the temperatures below zero to the running total
