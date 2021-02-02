0556ee27f19b2f4b72d5c0838c048fa4e4a624e3
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 17:01:02 -0500

    Update quickestimate.py

--- quickestimate.py
+++ quickestimate.py
@@ -1,2 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 for index in range(int(input())):
     print(len(input())) # In this context, "magnitude" refers to how many digits the number has
