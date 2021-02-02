0d0370f61fa8a1822c45148f57d9218c7b47e358
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:58:53 -0500

    Update pieceofcake2.py
    add license

--- pieceofcake2.py
+++ pieceofcake2.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Brute force solution, making all possible combinations of the cake's 2 cuts (with a height of 4 cm)
 
 n, h, v = map(int, input().split())
