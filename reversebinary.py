bee038958b8b2f659ccd8f02a0cd80ed1c13b553
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 17:01:32 -0500

    Update reversebinary.py

--- reversebinary.py
+++ reversebinary.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 N = bin(int(input())) # Convert input to binary
 N = N[2:]             # Remove 0b from beginning of number
 N = N[::-1]           # Reverse number
