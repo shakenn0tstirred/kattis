80dd6628c0d2a20740f776340d5e9c3f4f318086
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:56:26 -0500

    Update isithalloween.py

--- isithalloween.py
+++ isithalloween.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 date = input().split()
 
 if date[0] == "OCT" and date[1] == "31" or date[0] == "DEC" and date[1] == "25":
