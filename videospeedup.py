9cbb6af71e105ad150cedcaf33228643945c5814
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 17:03:25 -0500

    Update videospeedup.py

--- videospeedup.py
+++ videospeedup.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Get input for given variables
 n, p, k = map(int, input().split())
 t = list(map(int, input().split()))
