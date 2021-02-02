c984712f69d40939d3dc7eb5b9ce10b62f0a9f3b
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:57:10 -0500

    Update lastfactorialdigit.py

--- lastfactorialdigit.py
+++ lastfactorialdigit.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from math import factorial
 for i in range(int(input())):
     print(factorial(int(input())) % 10) # takes the last digit of an integer
