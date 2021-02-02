bc14d722d39a6e4d2c682ea20cf5436e6a9f5ef0
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 17:02:23 -0500

    Update speedlimit.py

--- speedlimit.py
+++ speedlimit.py
@@ -1,5 +1,8 @@
-# Kattis-accepted implementation
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+# Kattis-accepted implementation
 n = int(input())
 
 while n != -1:
