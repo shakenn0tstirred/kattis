5d60e31522ff295ed2e2c9bd59c42f9f7bcb38aa
destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2021-02-02 16:59:04 -0500

    Update pizza2.py

--- pizza2.py
+++ pizza2.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Since pi cancels itself out, I don't use it in this program.
 
 R,C = map(int, input().split())
